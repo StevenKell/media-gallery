7d4bee2db6dce2e189ae3d7edd53c89d67a790a5
StevenKell <steventkell@gmail.com>
2024-09-02 21:53:21 -0400

    Finally works.

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container fluid>
+        <VRow>
+            <VCol v-for="n in 12" cols="3">
+                <VCard>
+                    hello
+                    <VImg 
+                    :src="require('@/assets/images/callForSponsors.png')"
+                    >
+
+                    </VImg>
+                </VCard>
+            </VCol>
+        </VRow>
+    </v-container>
+</template>
